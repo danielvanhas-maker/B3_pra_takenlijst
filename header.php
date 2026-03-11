6b66514591cfe70acb2226c45ddc70207df8753b
TijsOB <tijsblokje@gmail.com>
2026-03-11 13:26:51 +0100

    css toegevoegd en task bij header gezet

--- header.php
+++ header.php
@@ -1,12 +1,19 @@
 <header>
-    <div class="container">
+    <div class="header-content">
+      <div class="wrapper">
+        <img src="<?php echo $base_url; ?>/images/logo-big-v3.png" alt="Het logo van DeveloperLand met een draaimolen, kasteel, achtbaan en tot slot een gezin op de voorgrond." class="logo hidden-on-sm">
+        
+      </div>
         <nav>
-            
-            <a href="../index.php">Home</a> |
-            <a href="../user/create.php">User Create</a>
-            <a href="../task/create.php">Task Create</a>
+            <a href="../index.php">Home</a> | 
+            <a href="../user/create.php">User Create</a> | 
+            <a href="../task/create.php">Task Create</a> |
+            <a href="../task/tasks.php">Tasks</a> | 
             <a href="../login.php">Inloggen</a>
         </nav>
-        
     </div>
+    
+        
+        
+    
 </header>
