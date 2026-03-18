cbb8cc367d4df79a0940ae57ddeb6cb4bbca60d6
TijsOB <tijsblokje@gmail.com>
2026-03-18 11:51:32 +0100

    edit pagina aangepast dropdown toegevoegd

--- task/edit.php
+++ task/edit.php
@@ -1,3 +1,4 @@
+<?php require_once __DIR__.'/../backend/config.php'; ?>
 <!doctype html>
 <html lang="nl">
 
@@ -25,16 +26,42 @@
         $message = $statement->fetch(PDO::FETCH_ASSOC);
         ?>
 
-        <!-- Formulier voor edit: -->
+        
             <form action="<?php echo $base_url; ?>/backend/taskController.php" method="POST">
             <input type="hidden" name="action" value="edit">
             <input type="hidden" name="id" value="<?php echo $id; ?>">
-            <input type="text" name="title" value="<?php echo $message['title']; ?>">
-            <textarea name="description"><?php echo $message['description']; ?></textarea>
-            <input type="text" name="department" value="<?php echo $message['department']; ?>">
-            <input type="text" name="status" value="<?php echo $message['status']; ?>">
-            <input type="submit" value="Verstuur melding">
+            <div class="form-group">
+            <label for="title">Title van taak:</label>    
+            <input type="text" name="title" value="<?php echo $message['title']; ?>"class="form-input">
+            </div>
+            <div class="form-group">
+            <label for="description">Beschrijving:</label>    
+            <input type="text" name="description" value="<?php echo $message['description']; ?>"class="form-input">
+            </div>  
+            <div class="form-group">  
+            <label for="department">Afdeling:</label>
+                <select name="department" id="department">
+                    <option value="personeel">Personeel</option>
+                    <option value="horeca">Horeca</option>
+                    <option value="techniek">Techniek</option>
+                    <option value="inkoop">Inkoop</option>
+                    <option value="klantenservice">Klantenservice</option>
+                    <option value="groen">Groen</option>
+                </select>
+            </div>  
+            <div class="form-group">
+            <label for="status">Status</label>      
+            <select name="status" id="status">
+                    <option value="Not Done">Not Done</option>
+                    <option value="In Review">In Review</option>
+                    <option value="Done">Done</option>
+                </select>
+            </div>  
+            <div class="form-submit">
+                <input type="submit" value="Pas melding aan" class="submit">
+            </div class="form-submit">
         </form>
+ 
     </div>  
 
 </body>
