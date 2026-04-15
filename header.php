0ec71899b4e26ea82c2072b04e55213976969375
TijsOB <tijsblokje@gmail.com>
2026-04-15 11:42:33 +0200

    login Renamed

--- header.php
+++ header.php
@@ -14,10 +14,10 @@ if(session_status() == PHP_SESSION_NONE){
                 <a href="<?php echo $base_url; ?>/task/tasks.php">Taken</a> |
                 <?php else: ?>
                 <a href="<?php echo $base_url; ?>/user/create.php">Gebruiker aanmaken</a> |
-                <a href="<?php echo $base_url; ?>/user/users.php">Gebruikers beheren</a>
+                <a href="<?php echo $base_url; ?>/user/users.php">Gebruikers beheren</a> |
                 <a href="<?php echo $base_url; ?>/task/tasksAdmin.php">Taken</a> |
                 <?php endif; ?>
-                <a href="<?php echo $base_url; ?>/task/create.php">Taak aanmaken</a>
+                <a href="<?php echo $base_url; ?>/task/create.php">Taak aanmaken</a> 
                 <?php if($_SESSION == TRUE): ?>
                 <p><a href="<?php echo $base_url; ?>/logout.php">Uitloggen</a></p>
                 <?php else: ?>
