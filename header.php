4ad236c0972048b6deefafe45f4ab845b8bf79d0
danielvanhassel <danielvanhassel@ziggo.nl>
2026-04-16 04:54:35 +0200

    updated header to make options available after login and removed login link as its the index

--- header.php
+++ header.php
@@ -10,26 +10,20 @@ if(session_status() == PHP_SESSION_NONE){
         
         </div>
             <nav>
-                <?php if (!isset($_SESSION['user_admin'])): ?>
-                <a href="<?php echo $base_url; ?>/task/tasks.php">Taken</a> |
-                <?php else: ?>
-                <a href="<?php echo $base_url; ?>/user/create.php">Gebruiker aanmaken</a> |
-                <a href="<?php echo $base_url; ?>/user/users.php">Gebruikers beheren</a> |
-                <a href="<?php echo $base_url; ?>/task/tasksAdmin.php">Taken</a> |
-                <?php endif; ?>
-                <a href="<?php echo $base_url; ?>/task/create.php">Taak aanmaken</a> 
-                <?php if($_SESSION == TRUE): ?>
-                <p><a href="<?php echo $base_url; ?>/logout.php">Uitloggen</a></p>
-                <?php else: ?>
-                <p><a href="<?php echo $base_url; ?>/index.php">Inloggen</a></p>
-                <?php endif; ?>
                 <?php if (isset($_SESSION['user_id'])): ?>
-                    <div>
-                        <a href="<?= $base_url; ?>/user/read.php?id=<?= htmlspecialchars($_SESSION['user_id']) ?>">
-                            Account inzien
-                        </a>
-                    </div>
+                    <?php if (!empty($_SESSION['user_admin'])): ?>
+                        <a href="<?php echo $base_url; ?>/user/create.php">Gebruiker aanmaken</a> |
+                        <a href="<?php echo $base_url; ?>/user/users.php">Gebruikers beheren</a> |
+                        <a href="<?php echo $base_url; ?>/task/tasksAdmin.php">Taken</a> |
+                    <?php else: ?>
+                        <a href="<?php echo $base_url; ?>/task/tasks.php">Taken</a> |
+                    <?php endif; ?>
+                    <a href="<?php echo $base_url; ?>/task/create.php">Taak aanmaken</a> |
+                    <a href="<?= $base_url; ?>/user/read.php?id=<?= htmlspecialchars($_SESSION['user_id']) ?>">
+                        Account inzien
+                    </a> |
+                    <a href="<?php echo $base_url; ?>/logout.php">Uitloggen</a>
                 <?php endif; ?>
-            </nav>
+                </nav>
         </div>
 </header>
